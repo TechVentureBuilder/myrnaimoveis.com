d0f665527a8178d04dd98900c0a1795ee0cdc936
Claudio Gomes <gomes.codes@gmail.com>
2022-03-25 13:43:24 -0300

    Fixed duplicated /form components folder.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import styled from 'styled-components'
 import Button from '../components/Button'
 import Container from '../components/Container'
-import TextInput from '../components/form/form/TextInput'
+import TextInput from '../components/form/TextInput'
 
 const Home: NextPage = (props) => {
   return (
