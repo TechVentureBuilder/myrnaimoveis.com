8e2399c9a3202df56c601e70761c5e1a78888edb
Claudio Gomes <gomes.codes@gmail.com>
2022-03-25 13:02:51 -0300

    Added Container to index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from 'next'
 import styled from 'styled-components'
 import Button from '../components/Button'
+import Container from '../components/Container'
 import TextInput from '../components/form/form/TextInput'
 
 const Home: NextPage = (props) => {
   return (
-    <div>
-      <h1>Home</h1>
+    <Container>
       <Button text='Teste' iconName='myrna' />
       <TextInput label='Email' placeholder='type your email here'/>
-    </div>
+    </Container>
   )
 }
 
