4f09ed9d41eaa4536ee0978f8f27a6519e5c0754
Claudio Gomes <gomes.codes@gmail.com>
2022-04-04 16:00:49 -0300

    Added fixes to Icon component, hero.jpg asset and the Features section to the index.tsx

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,16 +1,27 @@
 import React from "react"
 import { ReactSVG } from "react-svg"
+import styled from "styled-components"
 
 type Props = {
 	iconName: string
 	className?: string
+	size?: "small" | "big"
 }
+
+const StyledIcon = styled(ReactSVG)`
+	&.big {
+		svg {
+			height: ${(props) => props.theme.sizes.l};
+			width: ${(props) => props.theme.sizes.l};
+		}
+	}
+`
 
 const Icon = (props: Props) => {
 	return (
-		<ReactSVG
+		<StyledIcon
 			src={`/assets/icons/${props.iconName}.svg`}
-			className={props.className}
+			className={`${props.className} ${props.size == "big" ? "big" : ""}`}
 		/>
 	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,25 @@
 import type { NextPage } from "next"
 import styled from "styled-components"
 import Container from "../components/Container"
+import Icon from "../components/Icon"
 import Search from "../components/Search"
 
-const StyledHome = styled.div``
+const HeroBackground = styled.section`
+	background-image: url("assets/img/hero.jpg");
+	background-position: center;
+`
 
 const Hero = styled.div`
-	position: absolute;
-	top: 0;
-	left: 0;
+	background-color: ${(props) => props.theme.colors.card}bf;
 	width: 100%;
-	height: 100vh;
-	background-color: ${(props) => props.theme.colors.card};
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
 	gap: ${(props) => props.theme.sizes.m};
-	padding: ${(props) => props.theme.sizes.m};
+	padding: ${(props) => props.theme.sizes.xl} ${(props) => props.theme.sizes.m};
+	min-height: 75vh;
+	position: relative;
 	h1 {
 		text-align: center;
 	}
@@ -28,16 +30,73 @@ const SearchCard = styled.div`
 	padding: ${(props) => props.theme.sizes.m};
 `
 
+const FeaturesBackground = styled.section`
+	width: 100%;
+	padding: 0 ${(props) => props.theme.sizes.m};
+	background-color: ${(props) => props.theme.colors.card};
+`
+
+const FeaturesGroup = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	width: 100%;
+	@media (max-width: ${(props) => props.theme.screens.m}) {
+		grid-template-columns: 1fr;
+	}
+`
+
+const Feature = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: ${(props) => props.theme.sizes.xs};
+	padding: ${(props) => props.theme.sizes.xl};
+	text-align: center;
+`
+
 const Home: NextPage = (props) => {
 	return (
-		<StyledHome>
-			<Hero>
-				<h1>Sonhos para chamar de lar</h1>
-				<SearchCard>
-					<Search direction="row" />
-				</SearchCard>
-			</Hero>
-		</StyledHome>
+		<>
+			<HeroBackground>
+				<Hero>
+					<h1>Sonhos para chamar de lar</h1>
+					<SearchCard>
+						<Search direction="row" />
+					</SearchCard>
+				</Hero>
+			</HeroBackground>
+			<FeaturesBackground>
+				<Container>
+					<FeaturesGroup>
+						<Feature>
+							<Icon iconName="size" size="big"></Icon>
+							<h3>Imoveis Na Planta</h3>
+							<p>
+								Empreendimentos luxuosos e modernos, para o futuro que você
+								merece.
+							</p>
+						</Feature>
+						<Feature>
+							<Icon iconName="chat" size="big"></Icon>
+							<h3>Atendimento Eficiente</h3>
+							<p>
+								Seja por email, telefone ou whats, estamos sempre dispostos a
+								encontrar o que precisa.
+							</p>
+						</Feature>
+						<Feature>
+							<Icon iconName="local" size="big"></Icon>
+							<h3>Ótimas Localizações</h3>
+							<p>
+								Perto dos melhores shoppings, restaurantes, teatros, cinemas,
+								clubes e escolas.
+							</p>
+						</Feature>
+					</FeaturesGroup>
+				</Container>
+			</FeaturesBackground>
+		</>
 	)
 }
 
